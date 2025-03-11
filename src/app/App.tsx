6a5594401317988e8c005d8bfe4635a75f99df84
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-03-11 18:46:28 +0700

    add simple routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,26 @@
 import { useTranslation } from "react-i18next";
 import { LanguageSelect } from "../widgets/LanguageSelect/LanguageSelect";
 import "./style.css";
+import { Routes, Route } from "react-router";
+import { NotFoundPage } from "../pages/ErrorPages/NotFoundPage";
 
 function App() {
-
     const { t } = useTranslation();
 
     return (
-        <>
-            <h1>
-                {t('test')}
-            </h1>
-            <LanguageSelect />
-        </>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <div>
+                        <h1>{t("test")}</h1>
+                        <LanguageSelect />
+                    </div>
+                }
+            />
+
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
-import "./app/i18n.ts"
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app/App.tsx";
+import "./app/i18n.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Suspense fallback="loading...">
-      <App />
-    </Suspense>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <StrictMode>
+            <Suspense fallback="loading...">
+                <App />
+            </Suspense>
+        </StrictMode>
+    </BrowserRouter>
+);
